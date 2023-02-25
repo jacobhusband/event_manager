5de2a2b1802edc6536c87bf267c327b5015b9875
Jacob Husband <jakehusband2@gmail.com>
2023-02-25 09:36:48 -0800

    Add CSV class and import the data

---
+++ lib/event_manager.rb
@@ -0,0 +1,11 @@
+require 'csv'
+puts 'Event Manager Initialized!'
+
+if File.exist? "event_attendees.csv"
+  contents = CSV.open('event_attendees.csv', headers: true, header_converters: :symbol)
+  contents.each do |row|
+    name = row[:first_name]
+    zipcode = row[:zipcode]
+    puts "#{name}, #{zipcode}"
+  end
+end
