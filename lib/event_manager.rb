b41d322e7868bd21fec8b54922e0c3dbb78cc92d
Jacob Husband <jakehusband2@gmail.com>
2023-02-26 07:16:33 -0800

    Add zipcode data parsing

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,11 +1,24 @@
 require 'csv'
 puts 'Event Manager Initialized!'
 
-if File.exist? "event_attendees.csv"
+def clean_zipcode(zipcode)
+  if zipcode.nil?
+    '00000'
+  elsif zipcode.length < 5
+    zipcode.rjust(5, '0')
+  elsif zipcode.length > 5
+    zipcode[0..4]
+  else
+    zipcode
+  end
+end
+
+if File.exist? 'event_attendees.csv'
   contents = CSV.open('event_attendees.csv', headers: true, header_converters: :symbol)
   contents.each do |row|
     name = row[:first_name]
     zipcode = row[:zipcode]
+    zipcode = clean_zipcode(zipcode)
     puts "#{name}, #{zipcode}"
   end
 end
